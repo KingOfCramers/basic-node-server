7c86dbcdd7b543f998dfc0ad4b7c30d495dfe4ca
KingOfCramers <32515581+KingOfCramers@users.noreply.github.com>
2018-07-06 22:14:15 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var http = require("http");
+var fs = require("fs");
+
+function onRequest(request, response){
+
+    response.writeHead(200,{'Content-Type':'text/html'} );
+    fs.readFile("./index.html", null, function(err,data){ // null options.
+        if(err){
+            response.writeHead(404);
+            response.write("File not found.");
+        } else {
+            response.write(data); // The .write method lets you pass html files.
+        }
+        response.end(); // End the response.
+
+    });
+}
+
+http.createServer(onRequest).listen(3000, () => {
+      console.log("Listening on port 3000.");
+});
